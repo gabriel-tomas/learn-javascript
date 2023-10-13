19002b52bcca839c2c5746a55d4326b74076ec22
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-10-13 14:36:11 -0300

    Math metodos e atributos

---
+++ aula15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9;
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+//let num2 = Math.round(num1);
+
+//console.log(Math.max(1, 2, -3, -5, 5, 23, -9));
+//console.log(Math.min(1, 2, -3, -5, 5, 23, -9));
+//let aleatorio = Math.round(Math.random() * (20 - 10) + 10);
+//let num3 = Math.PI;
+//let num3 = Math.sqrt(9);
+//let num3 = Math.pow(2, 3);
+console.log(Boolean(100 / 0));
