67b2fd841d67c79275d37f8e98732bb0a3832e21
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-10-15 14:54:32 -0300

    arrays

---
+++ aula16/index.js
@@ -0,0 +1,16 @@
+let nomes = ["gabriel", "gomes", "tomas"];
+
+nomes.push("maria");
+nomes.push("joão");
+
+console.log(nomes instanceof Array);
+
+//delete nomes[0];
+//let removido = nomes.shift();
+//console.log(removido);
+//nomes.unshift("ana", "maria");
+//nomes.unshift("maria");
+//nomes.push("maria", "valeria");
+//nomes[1] = "maria";
+//nomes[nomes.length] = "valeria";
+//console.log(nomes[1]);
