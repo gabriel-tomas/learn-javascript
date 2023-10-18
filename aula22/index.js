8f63342ad60fb9b6f0f6292be777bd7482718d4c
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-10-18 16:16:27 -0300

    operadores lógicos

---
+++ aula22/index.js
@@ -0,0 +1,10 @@
+const nome1 = "gabriel";
+const nome2 = "gabriel";
+const idade1 = 19;
+const idade2 = 19;
+
+const expressaoAnd = nome1 === nome2 && idade1 === idade2;
+const expressaoOr = true || false || false;
+const expressaoNot = !true;
+
+console.log(expressaoNot);
