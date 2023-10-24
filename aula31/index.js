4d381fbbe016e918c66120b5f7d346c109173eb7
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-10-24 16:47:28 -0300

    diferenças entre var e let e hoisting

---
+++ aula31/index.js
@@ -0,0 +1,13 @@
+// const verdadeira = true;
+// let nome = "gabriel";
+// var nome2 = "gabriel";
+
+// if(verdadeira) {
+//     var nome2 = "gomes"; // redeclarando
+// }
+
+// console.log(nome2);
+
+console.log(nome);
+
+var nome = "gabriel";
