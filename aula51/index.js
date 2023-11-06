1e64e22b71e40ceee3b2344101beeed596033682
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-11-06 14:34:49 -0300

    retorno de funções avançado

---
+++ aula51/index.js
@@ -0,0 +1,11 @@
+function criaMultiplicador(multiplicador) {
+    return (n) => n * multiplicador;
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
