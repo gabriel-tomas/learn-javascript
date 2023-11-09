fb98d48482ff234b6b3fbf820ac7f0de93bbab42
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-11-09 16:32:20 -0300

    juntando arrays com concat e spread operator

---
+++ aula64/index.js
@@ -0,0 +1,7 @@
+const nums1 = [1, 2, 3];
+const nums2 = [4, 5, 6];
+const nums3 = [7, 8, 9];
+const nums4 = [10, 11, 12];
+const arrayFinal = [...nums1, ...nums2, ...nums3, ...nums4];
+
+console.log(arrayFinal);
