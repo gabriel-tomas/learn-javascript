01e174861b83146d67db9e01b5638bd2475c4247
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-12-04 13:57:48 -0300

    express nodemon

---
+++ node/aula7/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`<form action='/' method='POST'>
+            Nome do usuário: <input type='text' name='nome'>
+            <button type="submit">Enviar</button>
+            </form>`);
+})
+
+app.post("/", (req, res) => {
+    res.send("Form enviado");
+})
+
+app.get("/about", (req, res) => {
+    res.send("Você está no /about");
+})
+
+app.listen(3000, () => {
+    console.log("Acesse http://localhost:3000");
+    console.log("Executando na porta 3000");
+});
