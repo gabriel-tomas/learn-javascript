5b536d0075617408b5cf192b5bd245db920e7c35
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-01-11 14:44:49 -0300

    CRUD alunos

--- api_rest/src/controllers/AlunoController.js
+++ api_rest/src/controllers/AlunoController.js
@@ -5,6 +5,81 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json({ alunos });
   }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: ['ID missing'] });
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) return res.status(400).json({ errors: ['Aluno não existe'] });
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(401).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json({
+        alunoCreated: [aluno],
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: ['ID missing'] });
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) return res.status(400).json({ errors: ['Aluno não existe'] });
+
+      const novoAluno = await aluno.update(req.body);
+
+      return res.json({
+        alunoUpdated: [novoAluno],
+      });
+    } catch (e) {
+      return res.status(401).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: ['ID missing'] });
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) return res.status(400).json({ errors: ['Aluno não existe'] });
+
+      const alunoDeleted = await aluno.destroy();
+      return res.json({
+        alunoDeleted: [alunoDeleted],
+      });
+    } catch (e) {
+      return res.status(401).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- api_rest/src/routes/alunoRoutes.js
+++ api_rest/src/routes/alunoRoutes.js
@@ -1,7 +1,12 @@
 import alunoController from '../controllers/AlunoController';
+import loginRequired from '../middlewares/loginRequired';
 import { Router } from 'express'; // eslint-disable-line
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.get('/:id', alunoController.show);
+router.post('/', loginRequired, alunoController.create);
+router.put('/:id', loginRequired, alunoController.update);
+router.delete('/:id', loginRequired, alunoController.delete);
 
 export default router;
