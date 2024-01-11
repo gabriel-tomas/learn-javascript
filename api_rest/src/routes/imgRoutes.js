12fe9e7e895c466775965a121e57717064a5c31c
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-01-11 15:51:02 -0300

    filter files - only png and jpeg

--- api_rest/src/routes/imgRoutes.js
+++ api_rest/src/routes/imgRoutes.js
@@ -1,12 +1,8 @@
-import multer from 'multer';
 import { Router } from 'express'; // eslint-disable-line
 import imgController from '../controllers/ImgController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('image'), imgController.create);
+router.post('/', imgController.create);
 
 export default router;
