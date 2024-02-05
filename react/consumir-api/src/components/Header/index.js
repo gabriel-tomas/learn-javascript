94f924c6f3a1b70268884cc058207cf5974841f0
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-02-05 14:29:23 -0300

    react: logout do usuário

--- react/consumir-api/src/components/Header/index.js
+++ react/consumir-api/src/components/Header/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { FaHome, FaUserAlt, FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { FaHome, FaUserAlt, FaSignInAlt, FaUserCircle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+
+import * as actions from '../../store/modules/auth/actions';
+import history from '../../services/history';
 
 import Wrapper, { Nav } from './styled';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const iconSize = 17;
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+  };
 
   return (
     <Wrapper>
@@ -15,14 +27,29 @@ export default function Header() {
           <FaHome size={iconSize} />
           <span>Alunos</span>
         </Link>
-        <Link to="/register">
-          <FaUserAlt size={iconSize} />
-          <span>Registrar-se</span>
-        </Link>
-        <Link to="/login">
-          <FaSignInAlt size={iconSize} />
-          <span>Login</span>
-        </Link>
+        {!isLoggedIn ? (
+          <>
+            <Link to="/register">
+              <FaUserAlt size={iconSize} />
+              <span>Registrar-se</span>
+            </Link>
+            <Link to="/login">
+              <FaSignInAlt size={iconSize} />
+              <span>Login</span>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/register">
+              <FaUserAlt size={iconSize} />
+              <span>Editar usuário</span>
+            </Link>
+            <Link onClick={handleLogout} to="#">
+              <FaUserCircle size={iconSize} />
+              <span>Sair</span>
+            </Link>
+          </>
+        )}
       </Nav>
     </Wrapper>
   );
