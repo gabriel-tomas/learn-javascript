fa00412a8eb0e3625d4d575160b1922e4ff9248a
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-02-05 20:04:18 -0300

    react - adição de novas fotos - feature

--- react/consumir-api/src/pages/Fotos/index.js
+++ react/consumir-api/src/pages/Fotos/index.js
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { get } from 'lodash';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
 
-export default function Fotos() {
+import * as actions from '../../store/modules/auth/actions';
+import axios from '../../services/axios';
+import history from '../../services/history';
+
+import Loading from '../../components/Loading';
+
+import { Container, Title, Form } from './styled';
+
+export default function Fotos({ match }) {
+  const dispatch = useDispatch();
+
+  const id = get(match, 'params.id', false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [foto, setFoto] = useState('');
+
+  React.useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(`/alunos/${id}`);
+        const data = response.data;
+        setFoto(get(data, 'Fotos[0].url', ''));
+        setIsLoading(false);
+      } catch (err) {
+        toast.error('Erro ao obter imagem');
+        history.push('/');
+      }
+    }
+    getData();
+  }, []);
+
+  const handleChangeImg = async (e) => {
+    const file = e.target.files[0];
+    const fotoURL = URL.createObjectURL(file);
+
+    setFoto(fotoURL);
+
+    const formData = new FormData();
+    formData.append('aluno_id', id);
+    formData.append('image', file);
+
+    try {
+      setIsLoading(true);
+      await axios.post('/images/', formData, {
+        header: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success('Foto enviada com sucesso');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      console.log(err);
+      const { status } = get(err, 'response', '');
+      toast.error('Erro ao enviar a foto');
+
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
-    <div>
-      <h1>Fotos</h1>
-    </div>
+    <Container>
+      <Loading isLoading={isLoading} />
+      <Title>Fotos</Title>
+      <Form>
+        <label htmlFor="photo">
+          {foto ? (
+            <div className="container-img">
+              <img src={foto} alt="Foto" crossOrigin="" />
+            </div>
+          ) : (
+            <span>Selecionar foto</span>
+          )}
+          <input type="file" id="photo" onChange={handleChangeImg} />
+        </label>
+      </Form>
+    </Container>
   );
 }
+
+Fotos.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+};
--- react/consumir-api/src/pages/Fotos/styled.js
+++ react/consumir-api/src/pages/Fotos/styled.js
@@ -1,3 +1,53 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1``;
+import colors from '../../config/colors';
+
+export const Container = styled.div`
+  max-width: 500px;
+  margin-inline: auto;
+`;
+
+export const Title = styled.h1`
+  margin-top: 2rem;
+
+`;
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+
+  label {
+    width: 120px;
+    height: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #eee;
+    border: 5px dashed ${colors.primary};
+    border-radius: 50%;
+
+    .container-img {
+      width: 100%;
+      height: 100%;
+    }
+
+    .container-img > img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+  }
+
+  span {
+    color: ${colors.textDark};
+    font-weight: 500;
+    text-align: center;
+    padding: .5rem;
+  }
+
+  input {
+    display: none;
+  }
+`;
